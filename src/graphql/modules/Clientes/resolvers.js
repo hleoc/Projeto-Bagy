c05f21d33b612b339f3a5cef34a005b319c857b8
hleoc <informig@gmail.com>
2021-10-10 22:10:13 -0300

    Criado o servidor e o CRUD do Cliente

---
+++ src/graphql/modules/Clientes/resolvers.js
@@ -0,0 +1,65 @@
+const db = require("../../../db");
+
+function geradorDeId(lista) {
+  let novoId;
+  let ultimo = lista[lista.length - 1];
+  if (!ultimo) {
+    novoId = 0;
+  } else {
+    novoId = ultimo.id;
+  }
+
+  return novoId++;
+}
+
+module.exports = {
+  Query: {
+    cliente(_obj, args) {
+      return db.clientes.find((db) => db.id === args.id);
+    },
+    clientes: () => db.clientes,
+  },
+  Mutation: {
+    criarCliente(_obj, { data }) {
+      const { email } = data;
+
+      const clienteExistente = db.clientes.some((u) => u.email === email);
+
+      if (clienteExistente) {
+        throw new Error(`Cliente Existente: ${data.nome}`);
+      }
+
+      const novoCliente = {
+        ...data,
+        id: geradorDeId(db.clientes),
+      };
+      db.clientes.push(novoCliente);
+
+      return novoCliente;
+    },
+    atualizarCliente(_obj, { id, data }) {
+      const cliente = db.clientes.find((u) => u.id === id);
+      const indice = db.clientes.findIndex((u) => u.id === id);
+
+      const novoCliente = {
+        ...cliente,
+        ...data,
+      };
+      db.clientes.splice(indice, 1, novoCliente);
+      return novoCliente;
+    },
+    deletarCliente(_obj, { filtro: { id, email } }) {
+      if (id) {
+        const clienteEncontrado = db.clientes.find((u) => u.id === id);
+        db.clientes = db.clientes.filter((u) => u.id !== id);
+
+        return !!clienteEncontrado;
+      } else {
+        const clienteEncontrado = db.clientes.find((u) => u.email === email);
+        db.clientes = db.clientes.filter((u) => u.email !== email);
+
+        return !!clienteEncontrado;
+      }
+    },
+  },
+};
